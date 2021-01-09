e8ec6301bf924872af744bd3304219ce39329bb2
Edigleysson Silva <edigleyssonsilva@gmail.com>
2021-01-09 15:10:57 -0300

    feat: improve function call

--- google-cloud-function-with-scheduler/README.md
+++ google-cloud-function-with-scheduler/README.md
@@ -1,4 +1,7 @@
 
+
+
+
 ## Deploy Function
 
 `gcloud functions deploy drink_water_now --trigger-topic drink-water-now --runtime python37 --env-vars-file ./env-vars.yaml`
--- google-cloud-function-with-scheduler/main.py
+++ google-cloud-function-with-scheduler/main.py
@@ -6,10 +6,14 @@ def drink_water_now(event, context):
   This function remeMber a user to drink water periodically
   """
 
-  # data incomes in google cloud pub/sub funuction as a BASE64. UNCOMMENT the lines below to use it
+  # The data comes to the google cloud pub / sub function as a BASE64. Uncomment the following lines to use it
   # data = json.loads(base64.b64decode(event['data']).decode('utf-8'))
   # print(data)
 
-  send_mail('edigleyssonsilva@gmail.com', 'edigleyssonsilva@gmail.com', 'Keep Hydrated: Drink water now!', 'Hey, remember to drink water. It is very important for your health!!' , 'Edigleysson Silva')  
-
-
+  send_mail(
+    email_from='email_from@gmail.com',
+    to='email_to@gmail.com', 
+    subject='Keep Hydrated: Drink water now!',
+    body_message='Hey, remember to drink water. It is very important for your health!!',
+    from_name='Edigleysson Silva'
+  )
