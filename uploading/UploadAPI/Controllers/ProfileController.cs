971e193d7062f7fecb288e420c25651745b380c3
Edigleysson Silva <edigleyssonsilva@gmail.com>
2021-05-23 13:44:21 -0300

    feat: added health endpoint and new endpoint to multiple files

--- uploading/UploadAPI/Controllers/ProfileController.cs
+++ uploading/UploadAPI/Controllers/ProfileController.cs
@@ -36,5 +36,29 @@ namespace UploadAPI.Controllers
             });
         }
 
+
+        [HttpGet("health")]
+        public ActionResult<string> Health(){
+            return Ok("Backend is working");
+        }
+
+        [HttpPost("upload-mutiple")]
+        [Produces("application/json")]
+        [DisableRequestSizeLimit]
+        public ActionResult<string> UploadMultiple(
+            [FromForm(Name = "files")] List<IFormFile> files
+        ) {
+            if(files.Count == 0) return BadRequest();
+            List<string> data = new List<string>();
+            foreach(var file in files) {
+                data.Add($"Filename: {file.FileName} || Type: {file.ContentType}");
+            }
+
+            return Ok(new {
+                message =  $"Uploaded {files.Count} files",
+                files = data
+            });
+        }
+
     }
 }
